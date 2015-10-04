7db65ff9ffa188bcd3a92cfc161a4c3431125f26
coodie <poppinggoovie@gmail.com>
2015-10-04 23:52:09 +0200

    Removed run.sh script

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ main:
 	ghc  src/Main.hs -isrc --make -o hCompiler -outputdir build
 
 clean:
-	rm -R build
+	rm hCompiler -R build
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 import System.IO
 import System.Environment
+import System.Process
+import System.Exit
+import Data.List (init, dropWhileEnd)
 import Ast
 import Assembly
 import CParser
@@ -9,20 +12,42 @@ import Control.Monad
 
 main = do
     args <- getArgs
-    input <- getContents
+    input <- readFile (head args) 
     let parseResult = runCparser input
+    let justParse = elem "-t" args 
+    let rawFileName = init $ dropWhileEnd ( /= '.') (head args)
+    return ()
     case parseResult of
         Left error -> do
-            when (not $ elem "--no-error" args) $ putStr $ ppShow error
-        Right tree -> do
-            when (elem "-S" args) (dumpAssembly tree)
-            when (elem "-t" args) (dumpParseTree tree)
-    return ()
+            putStrLn $ ppShow error
+        Right tree -> 
+                if justParse then 
+                    dumpParseTree tree
+                else do
+                    writeFile (rawFileName ++ ".s") (runAssembly tree)
+                    runAs rawFileName
+                    runLinker rawFileName
 
 dumpParseTree :: ParseTree -> IO ()
 dumpParseTree tree = putStr $ ppShow tree
 
 dumpAssembly :: ParseTree -> IO ()
 dumpAssembly tree = putStr $ runAssembly tree
+
+runAs :: String -> IO ()
+runAs filename = do
+    let asArgs = [filename ++ ".s", "--32", "-o", filename ++ ".o"]
+    (asExit, asStdOut, asStdErr) <- readProcessWithExitCode "as" asArgs ""
+    case asExit of 
+        ExitSuccess -> return ()
+        ExitFailure _ -> putStr asStdOut
+
+runLinker :: String -> IO ()
+runLinker filename = do
+    let linkerArgs = ["-m32",filename ++ ".o", "-o", filename]
+    (linkerExit, linkerStdOut, linkerStdErr) <- readProcessWithExitCode "gcc" linkerArgs ""
+    case linkerExit of 
+        ExitSuccess -> return ()
+        ExitFailure _ -> putStr linkerStdOut
     
 
