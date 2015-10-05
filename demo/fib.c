7348aca7bffc6fd46c300297e83a489f3f2c555a
coodie <poppinggoovie@gmail.com>
2015-10-05 02:05:35 +0200

    Improved fib.c demo

--- demo/fib.c
+++ demo/fib.c
@@ -1,12 +1,38 @@
-int fib(int n)
+int fib_it(int n)
+{
+    int i = n;
+    int a = 0;
+    int b = 1;
+    int res = 0;
+    while(i >= 1)
+    {
+        res = a+b;
+        a = b;
+        b = res;
+        i = i-1;
+    }
+    return res;
+}
+
+int fib_rec(int n)
 {
     if(n <= 1)
-        return n;
+        return 1;
     else
-        return fib(n-1)+fib(n-2);
+        return fib_rec(n-1)+fib_rec(n-2);
 }
 
 int main()
 {
-    print_int(fib(17));
+    print_int(fib_it(1));
+    print_int(fib_it(2));
+    print_int(fib_it(3));
+    print_int(fib_it(4));
+    print_int(fib_it(5));
+
+    print_int(fib_rec(1));
+    print_int(fib_rec(2));
+    print_int(fib_rec(3));
+    print_int(fib_rec(4));
+    print_int(fib_rec(5));
 }
